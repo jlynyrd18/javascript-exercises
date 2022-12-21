267490488ea1a0a92f65fa77362de5d9c7155f54
Jordan Lenard <leonard_2010@live.com>
2022-12-20 21:33:56 -0600

    pass all test for years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(x) {
+    //if % is greater than 0  is not leap year?
+    //if % after being divided by 100 is 0 
+    if(x % 400 == 0 && x % 100 == 0) {
+        return true;
+    }else if (x % 400 !== 0 && x % 100 == 0) {
+        return false;
+    }else if (x % 4 == 0) {
+        return true;
+    }else {
+        return false;
+    }
 };
 
 // Do not edit below this line
