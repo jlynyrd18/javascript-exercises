a98a016498ba2bf8c2b7bb10dc6f505b06312a15
Jordan Lenard <leonard_2010@live.com>
2022-12-21 20:49:50 -0600

    filled both functions to convert to each other

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
-
+//.toFixed(1) for round to the tenth place?
+//ftoc form is f-32 divided by 1.800
+//ctof form is c*1.800 + 32
+const ftoc = function(x) {
+  let cDeg = (x - 32) / 1.800;
+  let rDeg = cDeg.toFixed(1);
+  return Number(rDeg);
 };
 
-const ctof = function() {
-
+const ctof = function(y) {
+  let fDeg = (y * 1.800) + 32;
+  let rDeg = fDeg.toFixed(1);
+  return Number(rDeg);
 };
 
 // Do not edit below this line
